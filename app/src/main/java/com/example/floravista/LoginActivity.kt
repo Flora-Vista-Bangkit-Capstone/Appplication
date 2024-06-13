a8d7a87957393b53524c4b0de42b022f7788f544
ahmad <azurenein@gmail.com>
2024-06-13 23:04:26 +0700

    depin afk

--- app/src/main/java/com/example/floravista/LoginActivity.kt
+++ app/src/main/java/com/example/floravista/LoginActivity.kt
@@ -1,11 +1,65 @@
 package com.example.floravista
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.example.floravista.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerNow.setOnClickListener{
+            val intent = Intent(this@LoginActivity,  SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        setupView()
+        playAnimation()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.welcomeImg, View.TRANSLATION_Y, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val welcome = ObjectAnimator.ofFloat(binding.textView, View.ALPHA, 1f).setDuration(500)
+        val email = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val pass = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val button = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(500)
+        val not = ObjectAnimator.ofFloat(binding.notMember, View.ALPHA, 1f).setDuration(500)
+        val reg = ObjectAnimator.ofFloat(binding.registerNow, View.ALPHA, 1f).setDuration(500)
+        AnimatorSet().apply {
+            playSequentially(welcome, email, pass, button, not, reg)
+            start()
+        }
+
     }
 }
--- app/src/main/java/com/example/floravista/WelcomeActivity.kt
+++ app/src/main/java/com/example/floravista/WelcomeActivity.kt
@@ -1,11 +1,56 @@
 package com.example.floravista
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.app.ActivityOptionsCompat
+import com.example.floravista.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.getButton.setOnClickListener{
+            val intent = Intent(this@WelcomeActivity,  LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        setupView()
+        playAnimation()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+
+    private fun playAnimation() {
+        val image = ObjectAnimator.ofFloat(binding.imageFloravista, View.ALPHA, 1f).setDuration(1000)
+        val button = ObjectAnimator.ofFloat(binding.getButton, View.ALPHA, 1f).setDuration(700)
+        AnimatorSet().apply {
+            playSequentially(image, button)
+            start()
+        }
     }
 }
